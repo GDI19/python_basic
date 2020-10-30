40328c72223661e802d0f05138b0b29c93ffa273
GDI19 <dimon.1982@mail.ru>
2020-10-30 17:58:47 +0300

    deleted les_1 change task_6

--- home_works/les_1/task_2.py
+++
@@ -1,7 +0,0 @@
-num = int(input('Введите число в секундах чтобы получить чч:мм:сс: '))
-
-hours = num // 3600
-minutes = (num % 3600) // 60
-seconds = (num % 3600) % 60
-
-print('Получаем {:02}:{:02}:{:02}'.format(hours, minutes, seconds))
--- home_works/les_1/task_4.py
+++
@@ -1,8 +0,0 @@
-num = input('Введите число: ')
-
-maxi = 0
-for i in range(len(num)-1):
-    if maxi < int(num[i]):
-        maxi = int(num[i])
-
-print('Максимальная цифра в заданом числе будет:', maxi)
